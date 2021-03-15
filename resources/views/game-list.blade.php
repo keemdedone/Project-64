fdeec6a0b5ddb98213880aff481a13464173b781
keemdedone <77421202+keemdedone@users.noreply.github.com>
2021-03-15 14:56:13 +0700

    update

--- resources/views/game-list.blade.php
+++ resources/views/game-list.blade.php
@@ -19,7 +19,7 @@
             @endif
                 <td>
                     <a href="{{ route('game-view', ['game' => $game->id]) }}">
-                        <img src="{{ asset("images/game/{$game['id']}.jpg") }}" alt="The image of {{ $game['name'] }}" style="width: 250px;" /><br/>
+                        <img src="{{ asset("images/game/g{$game['id']}.jpg") }}" alt="The image of {{ $game['name'] }}" style="width: 250px;" /><br/>
                         <h3>{{$game->name}}</h3>
                     </a>
                 </td>
