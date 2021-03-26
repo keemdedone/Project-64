f416309f181fe52d08ff82902e4085b0f96f6455
Phoominan622110206 <sankyu.p.39@gmail.com>
2021-03-26 13:09:25 +0700

    update

--- resources/views/select-create-review.blade.php
+++ resources/views/select-create-review.blade.php
@@ -8,14 +8,14 @@
 
 <table  border="0" style="width: 100%; ">
 <tr>
-    <td class="submit3" style="width: 50%;"> 
+    <td onclick="window.location='/game/create'" class="submit3" style="width: 50%;"> 
 
         @can('update',\App\Models\Game::class)
         <b ><a href="{{ route('game-create-form') }}" style="color: white; text-decoration: none; font-size: 300%;">Game</a> </b>
     
         @endcan
     </td>
-    <td class="submit3" style="width: 50%;" >
+    <td onclick="window.location='/manga/create'" class="submit3" style="width: 50%;" >
         @can('update',\App\Models\Manga::class)
         <b><a href="{{ route('manga-create-form') }}" style="color: white; text-decoration: none; font-size: 300%;">Manga</a> </b>
         @endcan
