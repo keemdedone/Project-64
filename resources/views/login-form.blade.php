53c80d335ebabc8bfb919671f31bb7fc6299c686
keemdedone <77421202+keemdedone@users.noreply.github.com>
2021-03-31 10:35:57 +0700

    clean code

--- resources/views/login-form.blade.php
+++ resources/views/login-form.blade.php
@@ -10,45 +10,45 @@
 
 <body>
     <form action="{{ route('authenticate') }}" method="post">
-    <table align="center" border="0" bo class="table-login" style="margin-top: 150px;">
-    <tr><td>
-    <h1 align="center">Longin</h1>
-    <table align="center" border="0" class=""  style="width: 500px;">
-        @csrf
-        <tr>
-            <td ><label>E-mail </td> 
-        </tr>
-        <tr>
-            <td><input type="text" name="email"  />
-            </label></td>
-        </tr>
-        <tr>
-            <td><label> Password </td>
-        </tr>
-        
-        <tr>
-            <td><input type="password" name="password" />
-            </label></td>
-        </tr>
+        <table align="center" border="0" bo class="table-login" style="margin-top: 150px;">
+            <tr>
+                <td>
+                    <h1 align="center">Longin</h1>
+                    <table align="center" border="0" class=""  style="width: 500px;">
+                        @csrf
+                        <tr>
+                            <td>E-mail </td> 
+                        </tr>
+                        <tr>
+                            <td>
+                                <input type="text" name="email"  />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td> Password </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <input type="password" name="password" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <button type="submit">Login</button>
+                            </td>
+                        </tr>
+                    </table>
 
-        <tr>
-        <td><button type="submit">Login</button></td>
-        </tr>
-        
-        </table>
-<table align="center" border="1" class="status-error" >
-     <tr>
-        @error('email')
-       <td><div>{{ $message }}</div></td>
-            @enderror
-       
-     </tr>
-        </table>
-   </td>
-</tr>
-   </table>
-            
-    </form>
-        
+                    <table align="center" border="1" class="status-error" >
+                        <tr>
+                            @error('email')
+                                <td><div>{{ $message }}</div></td>
+                            @enderror      
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>           
+    </form>      
 </body>
 </html>
