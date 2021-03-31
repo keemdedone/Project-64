9c8da9ebee3a9b8f122e91fef66b5954a342771e
keemdedone <77421202+keemdedone@users.noreply.github.com>
2021-03-31 10:28:35 +0700

    fix

--- resources/views/select-create-review.blade.php
+++ resources/views/select-create-review.blade.php
@@ -9,21 +9,14 @@
 <table  border="0" style="width: 100%; ">
 <tr>
     <td onclick="window.location='/game/create'" class="submit3" style="width: 50%;"> 
-
         @can('update',\App\Models\Game::class)
-        <b ><a href="{{ route('game-create-form') }}" style="color: white; text-decoration: none; font-size: 300%;">Game</a> </b>
-    
+            <b ><a href="{{ route('game-create-form') }}" style="color: white; text-decoration: none; font-size: 300%;">Game</a> </b>
         @endcan
     </td>
     <td onclick="window.location='/manga/create'" class="submit3" style="width: 50%;" >
         @can('update',\App\Models\Manga::class)
-        <b><a href="{{ route('manga-create-form') }}" style="color: white; text-decoration: none; font-size: 300%;">Manga</a> </b>
+            <b><a href="{{ route('manga-create-form') }}" style="color: white; text-decoration: none; font-size: 300%;">Manga</a> </b>
         @endcan
-
-    </td>
-</tr>
-</table> 
-  
     </td>
 </tr>
 </table>
